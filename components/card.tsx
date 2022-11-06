ee7587e9bf58b659042d129c029dc357e9830a28
Se1roon <se1roonm@gmail.com>
2022-11-06 11:39:01 +0100

    Add font awesome and ratings

--- components/card.tsx
+++ components/card.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./css/Card.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 interface CardProps {
   record: {
@@ -14,6 +17,25 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ record }): JSX.Element => {
+  const getRating = (): JSX.Element => {
+    const fullStars = Math.floor(record.rating);
+    const stars = new Array(fullStars).fill(0);
+
+    if (record.rating > fullStars) stars.push(1);
+
+    return (
+      <>
+        {stars.map((star, index) => {
+          return star ? (
+            <FontAwesomeIcon key={index} icon={faStarHalf as IconProp} />
+          ) : (
+            <FontAwesomeIcon key={index} icon={faStar as IconProp} />
+          );
+        })}
+      </>
+    );
+  };
+
   return (
     <div className={styles.card}>
       <div className={styles.card_content}>
@@ -33,11 +55,7 @@ const Card: React.FC<CardProps> = ({ record }): JSX.Element => {
         </div>
 
         <div className={styles.rating}>
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-regular fa-star"></i>
-          <i className="fa-regular fa-star"></i>
+          {record.rating ? getRating() : null}
         </div>
 
         <div className={styles.button}>
