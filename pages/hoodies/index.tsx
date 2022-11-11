d069c896f5da76bf9f2a3a82493f80228658b905
Se1roon <se1roonm@gmail.com>
2022-11-11 12:37:03 +0100

    Load data from hoodies collection

--- pages/hoodies/index.tsx
+++ pages/hoodies/index.tsx
@@ -2,7 +2,21 @@ import Head from "next/head";
 import React from "react";
 import Layout from "./../../components/layout";
 
-const Home: React.FC = (): JSX.Element => {
+interface HomeProps {
+  records: {
+    id: string;
+    name: string;
+    path_to_image: string;
+    price: number;
+    rating: number;
+    sizes: string;
+    bestseller: boolean;
+  }[];
+}
+
+const Home: React.FC<HomeProps> = ({ records }): JSX.Element => {
+  console.log(records);
+
   return (
     <Layout>
       <Head>
@@ -11,5 +25,20 @@ const Home: React.FC = (): JSX.Element => {
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  const res = await fetch(
+    `http://127.0.0.1:8090/api/collections/hoodies/records?page=1&perPage=30`
+  );
+
+  let data = await res.json();
+  data = data?.items as any[];
+
+  return {
+    props: {
+      records: data,
+    },
+  };
+}
 
 export default Home;
