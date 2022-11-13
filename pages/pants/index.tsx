5d1f7ff962cc8799421ca7d72a5155859902c739
Se1roon <se1roonm@gmail.com>
2022-11-13 09:56:50 +0100

    Add Pants route

---
+++ pages/pants/index.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import React from "react";
+import Layout from "./../../components/layout";
+import Clothes from "../../components/clothes";
+
+interface HomeProps {
+  records: {
+    id: string;
+    name: string;
+    path_to_image: string;
+    price: number;
+    rating: number;
+    sizes: string;
+    bestseller: boolean;
+  }[];
+}
+
+const Home: React.FC<HomeProps> = ({ records }): JSX.Element => {
+  return (
+    <Layout>
+      <Head>
+        <title>Hoodies</title>
+      </Head>
+      <Clothes records={records} />
+    </Layout>
+  );
+};
+
+export async function getStaticProps() {
+  const res = await fetch(
+    `http://127.0.0.1:8090/api/collections/pants/records?page=1&perPage=30`
+  );
+
+  let data = await res.json();
+  data = data?.items as any[];
+
+  return {
+    props: {
+      records: data,
+    },
+  };
+}
+
+export default Home;
