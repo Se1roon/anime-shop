0acd98bbca591854337054191daab52b48754c23
Se1roon <se1roonm@gmail.com>
2022-11-13 09:43:57 +0100

    Implement records filtering based on min and max price

--- components/clothes.tsx
+++ components/clothes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "./grid";
 import styles from "./css/Clothes.module.css";
 import Filter from "./filter";
@@ -16,10 +16,34 @@ interface ClothesProps {
 }
 
 const Clothes: React.FC<ClothesProps> = ({ records }): JSX.Element => {
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(299.99);
+
+  const updateMinPrice = (value) => {
+    setMinPrice(value);
+  };
+
+  const updateMaxPrice = (value) => {
+    setMaxPrice(value);
+  };
+
+  const filterRecords = () => {
+    const filtered = records.filter(
+      (record) => record.price >= minPrice && record.price <= maxPrice
+    );
+
+    return filtered;
+  };
+
   return (
     <section className={styles.section}>
-      <Filter />
-      <Grid records={records} />
+      <Filter
+        maxPrice={maxPrice}
+        minPrice={minPrice}
+        setMinPrice={updateMinPrice}
+        setMaxPrice={updateMaxPrice}
+      />
+      <Grid records={filterRecords()} />
     </section>
   );
 };
--- components/filter.tsx
+++ components/filter.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./css/Filter.module.css";
 
-const Filter: React.FC = (): JSX.Element => {
+interface FilterProps {
+  minPrice: number;
+  maxPrice: number;
+  setMinPrice: (value: number) => void;
+  setMaxPrice: (value: number) => void;
+}
+
+const Filter: React.FC<FilterProps> = ({
+  minPrice,
+  maxPrice,
+  setMinPrice,
+  setMaxPrice,
+}): JSX.Element => {
   return (
     <section className={styles.section}>
       <ul className={styles.list}>
         <li className={styles.item}>
           <label htmlFor="minPrice">Min Price</label>
-          <input type="number" name="minPrice" id="minPrice" />
+          <input
+            type="number"
+            name="minPrice"
+            id="minPrice"
+            value={minPrice}
+            onChange={(e) => setMinPrice(Number(e.target.value))}
+          />
         </li>
         <li className={styles.item}>
           <label htmlFor="maxPrice">Max Price</label>
-          <input type="number" name="maxPrice" id="maxPrice" />
+          <input
+            type="number"
+            name="maxPrice"
+            id="maxPrice"
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
         </li>
       </ul>
     </section>
