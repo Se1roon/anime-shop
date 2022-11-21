62337a94fb403548bf7314fd50618cbe3c63075c
Se1roon <se1roonm@gmail.com>
2022-11-21 14:28:23 +0100

    Add styled components for Filter

--- components/filter.tsx
+++ components/filter.tsx
@@ -1,6 +1,56 @@
 import React, { useState } from "react";
-import styles from "./css/Filter.module.css";
+import styled from "styled-components";
 
+// Styles
+
+const Section = styled.section`
+  font-family: "Montserrat", sans-serif;
+  color: var(--bg-light);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media (min-width: 1000px) {
+    margin-top: 6rem;
+    padding-inline: 4rem;
+    align-self: flex-start;
+    position: sticky;
+    top: 20%;
+  }
+
+  @media (min-width: 1100px) {
+    padding-left: 0;
+  }
+`;
+
+const List = styled.ul`
+  list-style: none;
+`;
+
+const ListItem = styled.li`
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  font-size: 1.3rem;
+  margin-bottom: 0.2rem;
+`;
+
+const Input = styled.input`
+  font-family: "Montserrat", sans-serif;
+  font-size: 0.9rem;
+  color: var(--bg-light);
+  background: transparent;
+  outline: 0;
+  padding: 0.3rem 1rem;
+  border: 2px solid var(--clr-accent);
+  border-radius: 1rem;
+`;
+
+// ==============================
 interface FilterProps {
   minPrice: number;
   maxPrice: number;
@@ -15,30 +65,32 @@ const Filter: React.FC<FilterProps> = ({
   setMaxPrice,
 }): JSX.Element => {
   return (
-    <section className={styles.section}>
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <label htmlFor="minPrice">Min Price</label>
-          <input
+    <Section>
+      <List>
+        <ListItem>
+          <Label htmlFor="minPrice">Min Price</Label>
+          <Input
+            as="input"
             type="number"
             name="minPrice"
             id="minPrice"
             value={minPrice}
             onChange={(e) => setMinPrice(Number(e.target.value))}
           />
-        </li>
-        <li className={styles.item}>
-          <label htmlFor="maxPrice">Max Price</label>
-          <input
+        </ListItem>
+        <ListItem>
+          <Label htmlFor="maxPrice">Max Price</Label>
+          <Input
+            as="input"
             type="number"
             name="maxPrice"
             id="maxPrice"
             value={maxPrice}
             onChange={(e) => setMaxPrice(Number(e.target.value))}
           />
-        </li>
-      </ul>
-    </section>
+        </ListItem>
+      </List>
+    </Section>
   );
 };
 
